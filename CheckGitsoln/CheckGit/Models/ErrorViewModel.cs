5958dd1e799002552efbc04b84e5c054e53cda3a
thejasv <48196639+thejasv@users.noreply.github.com>
2021-03-23 10:51:25 +0530

    my file

---
+++ CheckGitsoln/CheckGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CheckGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
